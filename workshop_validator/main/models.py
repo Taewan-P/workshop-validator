7abab56434ef155caebaada7a99497727e03eb1c
Taewan Park <swimingtw@gmail.com>
2021-01-16 18:33:48 +0900

    Add Member model

--- workshop_validator/main/models.py
+++ workshop_validator/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    username = models.CharField(max_length=50)
+    q1 = models.BooleanField(default=False)
+    q2 = models.BooleanField(default=False)
+    q3 = models.BooleanField(default=False)
+    q4 = models.BooleanField(default=False)
+    q5 = models.BooleanField(default=False)
+    q6 = models.BooleanField(default=False)
+    q7 = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
