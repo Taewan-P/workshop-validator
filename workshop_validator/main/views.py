77e1657ceeaaffb40ae58786f0addb5256595b57
Taewan Park <swimingtw@gmail.com>
2021-01-16 21:25:54 +0900

    Automatically render question templates depending on their progress

--- workshop_validator/main/views.py
+++ workshop_validator/main/views.py
@@ -18,14 +18,22 @@ def main_page(request):
             try:
                 mem_lookup = Member.objects.get(username=username)
             except Member.DoesNotExist:
+                # New member
                 new_member = Member(username=username)
                 new_member.save()
-                return render(request, 'main/two.html')
+                return render(request, 'main/question1.html', model_to_dict(new_member))
             
             # Already registered. -> Redirect to unsolved question
             values = model_to_dict(mem_lookup)
+            steps = ""
+            for q in ["q1", "q2", "q3", "q4", "q5", "q6", "q7"]:
+                if not values.get(q):
+                    # The question user has to solve
+                    print("Going to {0}".format(q))
+                    return render(request, 'main/{0}uestion{1}.html'.format(q[0], q[1]))
+
             print(values)
-            return render(request, 'main/two.html')
+            return render(request, 'main/two.html', values)
         else:
             # 404
             print("INVALID USERNAME")
