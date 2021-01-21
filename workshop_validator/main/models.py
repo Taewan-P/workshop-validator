9fd0ca539881d736eed2ffb3833067257223f41c
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-01-21 15:47:47 +0900

    Add 3 questions and move question order

--- workshop_validator/main/models.py
+++ workshop_validator/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Member(models.Model):
     username = models.CharField(max_length=50)
@@ -10,6 +11,9 @@ class Member(models.Model):
     q5 = models.BooleanField(default=False)
     q6 = models.BooleanField(default=False)
     q7 = models.BooleanField(default=False)
+    q8 = models.BooleanField(default=False)
+    q9 = models.BooleanField(default=False)
+    q10 = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
